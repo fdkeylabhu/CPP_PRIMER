2bc7893e45e48111fedbebe35ad1ab3769d26e5f
fdkeylabhu <44796297+fdkeylabhu@users.noreply.github.com>
2019-11-11 20:29:57 +0800

    Create answer_10_1.cpp

---
+++ 第10章-泛型算法/answer_10_1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <list>
+#include <numeric>
+
+using namespace std;
+
+int main()
+{
+
+    vector<int> a = {1, 1, 1, 3, 2, 3, 2};
+    std::cout << count(a.cbegin(), a.cend(), 3) << std::endl;
+
+    list<string> b;
+    string s;
+    while ( cin >> s ) {
+        b.push_back(s);
+    }
+    
+    std::cout << count(b.cbegin(), b.cend(), "sss") << std::endl;
+    return 0;
+
+}
